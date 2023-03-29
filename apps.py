05207dce1373cf08c37e58a31eea1d3a56e002ff
xkzldks <naochugu@gmail.com>
2023-03-30 06:07:26 +0900

    Add Text File [apps.py]

--- apps.py
+++ apps.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from pymongo import MongoClient
+client = pymongo.MongoClient("mongodb+srv://test:test@cluster1.9pincbb.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+db = client.dbstreamlit
+
+d = list(db.chulseck.find({}, {'_id': False}))
+d
 view = [100,150,30]
 st.write('# asdf')
 st.write('## rasd')
