ed9f0cc041879821196c7a60dc2fff8be6e98595
xkzldks <naochugu@gmail.com>
2023-03-30 17:08:32 +0900

    Add Text File [apps.py]

--- apps.py
+++ apps.py
@@ -1,5 +1,4 @@
 import streamlit as st
-<<<<<<< HEAD
 import pandas as pd
 from modules.data_query import DataQuery
 from modules.utils import run_concurrent_queries, estimate_gigabytes_scanned
@@ -14,14 +13,7 @@ from modules.sql_queries import (
 from google.cloud import bigquery
 from modules.plot import plot_pol, plot_temp, plot_prc
 import urllib
-=======
-from pymongo import MongoClient
-# client = pymongo.MongoClient("mongodb+srv://test:test@cluster1.9pincbb.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
-# db = client.dbstreamlit
 
-d = list(db.chulseck.find({}, {'_id': False}))
-d
->>>>>>> 53acc03d71ea2bfc82ee3512362cfa2769d2e9a3
 view = [100,150,30]
 st.write('# asdf')
 st.write('## rasd')
